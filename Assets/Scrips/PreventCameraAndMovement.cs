0beee7f8d6a021597533397359c233c306c73c6b
Kevin Fox <kevin@substantial.com>
2015-08-06 17:46:55 -0700

    fix camera

---
+++ Assets/Scrips/PreventCameraAndMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PreventCameraAndMovement : NetworkBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		//intercept camerea and controls
+		if (!isLocalPlayer) {
+//			GetComponent<CharacterMotor>().enabled = false;
+
+			Camera cam = GetComponentInChildren<Camera> ();
+			cam.enabled = false;
+		}
+	}
+}
